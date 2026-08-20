ea98b207b045ab494be1ec24c4c5bab55449bd48
Towid1 <mlopezbarros68@gmail.com>
2026-08-19 22:12:05 -0300

    Delete Semana 10 While directory

--- Semana 10 While/#EXcontagemsimples.py
+++
@@ -1,8 +0,0 @@
-#EXcomtagemsimples
-
-numero = 10
-contador = 1
-
-while contador <= 10:
-    print(contador)
-    contador += 1
--- Semana 10 While/#EXtabuadacomcontroledeusuario.py
+++
@@ -1,9 +0,0 @@
-#EXtabuadacomcontroledeusuario
-
-numero = int(input(":"))
-contador = 1
-
-while contador <= 10:
-    result = numero * contador
-    print(f"{numero} X {contador} = {result}")
-    contador += 1
